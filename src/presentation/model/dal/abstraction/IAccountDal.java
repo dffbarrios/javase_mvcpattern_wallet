07ebb0c4109d81854bf4862f5a6ad67ec11eb07b
dfbarrios <dfbarrios@gmail.com>
2018-09-09 13:27:33 -0500

    Dal abstraction and implementation scheme created

---
+++ src/presentation/model/dal/abstraction/IAccountDal.java
@@ -0,0 +1,9 @@
+package presentation.model.dal.abstraction;
+
+/**
+ *
+ * @author Diego SNP3004EX
+ */
+public interface IAccountDal {
+    
+}
---
+++ src/presentation/model/dal/abstraction/ICategoryDal.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package presentation.model.dal.abstraction;
+
+/**
+ *
+ * @author Diego SNP3004EX
+ */
+public interface ICategoryDal {
+    
+}
